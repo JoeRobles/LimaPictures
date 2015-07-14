601fd71dbf9d2361352349506ec3ff0c393df242
Joe Robles <joe.robles.pdj@gmail.com>
2015-07-14 11:40:13 -0700

    details created

--- Nature Portfolio/ViewController.swift
+++ Nature Portfolio/ViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableMain: UITableView?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as? UITableViewCell
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        }
+        var label_title : UILabel? = self.view.viewWithTag(1) as? UILabel;
+        label_title?.text = "Sample text from code"
+        
+        var label_thumbnail : UIImageView? = self.view.viewWithTag(2) as? UIImageView;
+        label_thumbnail?.image = UIImage(named: "monkLogo250")
+        
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
 }
 
